d8190ac21aa27670d3447410b65bd33730cecb7d
jja6312 <127708710+jja6312@users.noreply.github.com>
2024-10-26 23:48:33 +0900

    Presentation Layer테스트(1)

--- src/main/java/sample/cafekiosk/spring/api/controller/product/ProductController.java
+++ src/main/java/sample/cafekiosk/spring/api/controller/product/ProductController.java
@@ -2,9 +2,12 @@ package sample.cafekiosk.spring.api.controller.product;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import sample.cafekiosk.spring.api.controller.product.dto.ProductCreateRequest;
 import sample.cafekiosk.spring.api.service.product.ProductService;
-import sample.cafekiosk.spring.api.controller.product.response.ProductResponse;
+import sample.cafekiosk.spring.api.service.product.response.ProductResponse;
 
 import java.util.List;
 
@@ -12,6 +15,11 @@ import java.util.List;
 @RestController
 public class ProductController {
     private final ProductService productService;
+
+    @PostMapping("/api/v1/products/new")
+    public void createProduct(ProductCreateRequest request){
+        productService.createProduct(request);
+    }
 
     @GetMapping("/api/v1/products/selling")
     public List<ProductResponse> getSellingProducts(){
--- src/test/java/sample/cafekiosk/spring/domain/product/ProductRepositoryTest.java
+++ src/test/java/sample/cafekiosk/spring/domain/product/ProductRepositoryTest.java
@@ -1,17 +1,14 @@
 package sample.cafekiosk.spring.domain.product;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
 import static sample.cafekiosk.spring.domain.product.ProductSellingStatus.*;
 import static sample.cafekiosk.spring.domain.product.ProductType.*;
 
@@ -25,15 +22,9 @@ class ProductRepositoryTest {
 
     @DisplayName("원하는 판매 상태를 가진 상품들을 조회한다.")
     @Test
-    void findAllBuSelliongStatusIn(){
+    void findAllBuSelliongStatusIn() {
         // given
-        Product product1 = Product.builder()
-                .productNumber("001")
-                .type(HANDMADE)
-                .sellingStatus(SELLING)
-                .name("아메리카노")
-                .price(4000)
-                .build();
+        Product product1 = createProduct("001", HANDMADE, SELLING, "아메리카노", 4000);
         Product product2 = Product.builder()
                 .productNumber("002")
                 .type(HANDMADE)
@@ -74,15 +65,9 @@ class ProductRepositoryTest {
 
     @DisplayName("상품번호 리스트로 상품들을 조회한다.")
     @Test
-    void findAllByProductNumberIn(){
+    void findAllByProductNumberIn() {
         // given
-        Product product1 = Product.builder()
-                .productNumber("001")
-                .type(HANDMADE)
-                .sellingStatus(SELLING)
-                .name("아메리카노")
-                .price(4000)
-                .build();
+        Product product1 = createProduct("001", HANDMADE, SELLING, "아메리카노", 4000);
         Product product2 = Product.builder()
                 .productNumber("002")
                 .type(HANDMADE)
@@ -111,6 +96,46 @@ class ProductRepositoryTest {
                         tuple("001", "아메리카노", SELLING),
                         tuple("002", "팥빙수", STOP_SELLING)
                 );
+    }
+
+    @DisplayName("가장 마지막으로 저장한 상품의 상품 번호를 불러 온다.")
+    @Test
+    void findLatestProductNumber() {
+        // given
+        String targetProductNumber = "003";
+
+        Product product1 = createProduct("001", HANDMADE, SELLING, "아메리카노", 4000);
+        Product product2 = createProduct("002", HANDMADE, STOP_SELLING, "팥빙수", 7000);
+        Product product3 = createProduct(targetProductNumber, HANDMADE, HOLD, "카페라떼", 4500);
+        productRepository.saveAll(List.of(product1, product2, product3));
+
+        // when
+        String latestProductNumber = productRepository.findLatestProductNumber();
+
+
+        assertThat(latestProductNumber).isEqualTo("003");
+    }
+
+    @DisplayName("가장 마지막으로 저장한 상품 번호를 읽어올 떄, 상품이 하나도 없는 경우 null을 반환한다.")
+    @Test
+    void findLatestProductNumberWhenProductIsEmpty() {
+        //given
+        //when
+        String latestProductNumber = productRepository.findLatestProductNumber();
+
+        //then
+        assertThat(latestProductNumber)
+                .isNull();
+    }
+
+    private Product createProduct(String productNumber, ProductType type, ProductSellingStatus sellingStatus, String name, int price) {
+        return Product.builder()
+                .productNumber(productNumber)
+                .type(type)
+                .sellingStatus(sellingStatus)
+                .name(name)
+                .price(price)
+                .build();
     }
 
 
